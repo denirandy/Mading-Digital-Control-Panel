b646c0d02cbd0a7f34f87d283182ced2a61bc620
denirandy <reza2309reza@gmail.com>
2023-10-04 15:16:18 +0700

    nambahin sidebar

---
+++ controlPanel.js
@@ -0,0 +1,30 @@
+const layarMerah = document.getElementById("layar-merah")
+
+function changcePotitionLayarMerah(id) {
+    switch (id) {
+        case 4:
+            hapus()
+            layarMerah.classList.add("select-jadwal")
+            break
+        case 3:
+            hapus()
+            layarMerah.classList.add("select-vidio")
+            break
+        case 2:
+            hapus()
+            layarMerah.classList.add("select-text")
+            break
+        case 1:
+        default:
+            hapus()
+            layarMerah.classList.add("select-qrcode")
+            break
+    }
+    function hapus() {
+        layarMerah.classList.remove("select-qrcode")
+        layarMerah.classList.remove("select-text")
+        layarMerah.classList.remove("select-vidio")
+        layarMerah.classList.remove("select-jadwal")
+    }
+
+}
